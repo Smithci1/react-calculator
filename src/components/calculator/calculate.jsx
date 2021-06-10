23b1f601fffdcfbf97ba3887fc554699055cca07
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-06-10 18:14:39 -0400

    I was told to export from the top but I am having a hard time doing that

--- src/components/calculator/calculate.jsx
+++ src/components/calculator/calculate.jsx
@@ -1,10 +1,54 @@
 import React, { useState } from 'react'
+import './calcstyle.css'
 
-export default () => {
+// export default () => {
     // declare state
-    const [input1, setInput1] = useState('')
-    const [input2, setInput2] = useState('')
-    const [operator, setOperator] = useState('*')
-    const [result, setResult] = useState('')
-    const [error, setError] = useState('')
-}
+    // const [input1, setInput1] = useState('')
+    // const [input2, setInput2] = useState('')
+    // const [operator, setOperator] = useState('*')
+    // const [result, setResult] = useState('')
+    // const [error, setError] = useState('')
+
+
+    const Calculate = () => {
+        // if (operator === "times") {
+        //     return setResult(Number(input1) * (Number(input2))
+        // }
+       
+       
+        return (
+            <div className="calc">
+                <form>
+                    <input className="val1" type="text"/>
+                </form>
+
+            <select className="op">
+                <option value="times">*</option>
+                <option value="divide">÷</option>
+                <option value="add">+</option>
+                <option value="minus">-</option>
+            </select>
+
+                <form>
+                    <input className="val2" type="text" />
+                </form>
+                <form>
+
+                    <input className="equal" type="button" value="="></input>
+                </form>
+
+                <form>
+                    <input className="result" readOnly/>
+                </form>
+            </div>
+        )
+            
+        
+            
+        }
+    
+
+
+    
+
+export default Calculate
---
+++ src/components/title/title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './title.css'
+
+const Title = () => {
+    return (
+
+        <div className="title">
+    <h1>React Calculator</h1>
+  </div>
+        )
+}
+export default Title
