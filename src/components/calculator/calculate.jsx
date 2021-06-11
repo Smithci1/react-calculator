bc7fcf5f326fd7c800208c14763b79787b85f157
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-06-10 21:56:45 -0400

    okay.. so i believe the calculator works 100% how it should, will now fix the message css

--- src/components/calculator/calculate.jsx
+++ src/components/calculator/calculate.jsx
@@ -2,62 +2,44 @@
 import React, { useState } from 'react'
 import './calcstyle.css'
 
-// export () => {
-    // declare state
     export default () => {
-    
     
         const [input1, setInput1] = useState('')
         const [input2, setInput2] = useState('')
         const [operator, setOperator] = useState('')
         const [result, setResult] = useState('')
         const [error, setError] = useState('')
-        
-
-        // const invalidInputs = () => { 
-        //     return (
-        // (!Number || Number(input1) !==  &&
-        //         !Number || Number(input2) !== '') ) 
-        // }
-                    
+    
+    const invalidInputs = () => !Number.isNaN(Number(input1)) 
+    && !Number.isNaN(Number(input2)) &&
+        input1 !== '' &&
+        input2 !== '' 
+                     
 
     const Calculator = () => {
-                if (operator === "minus"){
-                  
-                  return setResult(Number(input1) - Number(input2))
-                 
-                }else if (operator ===  "times"){
-                    return setResult(Number(input1) * Number(input2))
-                    
-                }else if (operator === "divide"){
-                    return setResult(Number(input1) / Number(input2))
-                    
-                }else if (operator === "add"){
-                    return setResult(Number(input1) + Number(input2))
-                    
-                } 
-                else if (operator === '') {
-                     return setError( 'please select an operator') 
+        if (!invalidInputs()){
+            setResult('')
+            return setError('please provide a # for each input')    
+        }    
+            else if (operator === "minus"){  
+                return setResult(Number(input1) - Number(input2)) 
+        }   else if (operator ===  "times"){
+                return setResult(Number(input1) * Number(input2))            
+        }   else if (operator === "divide"){
+                return setResult(Number(input1) / Number(input2))            
+        }   else if (operator === "add"){
+                return setResult(Number(input1) + Number(input2))            
+        } 
+            else if (operator === '') {
+                return setError( 'please select an operator') 
+        } 
 
-                 } 
-             else if(!Number.isNaN(Number(input1)) && !Number.isNaN(Number(input2)) &&
-                      input1 !== '' &&
-                       input2 !== '') {
-                           setResult('')
-                      return setError('please provide a # for each input')
-                      } 
     }               
-            
-    
-    
-        
-        
         return (
             <div className="calc">
                 <form>
                     <input className="val1" value={input1} onChange={event => setInput1(event.target.value)} type="text"/>
                 </form>
-
             <select className="op" value={operator} onChange={event => setOperator(event.target.value)}>
                 <option value=''></option>
                 <option value="times">*</option>
@@ -70,10 +52,8 @@ import './calcstyle.css'
                     <input className="val2" value={input2} onChange={event => setInput2(event.target.value)} type="text" />
                 </form>
                 <form>
-
                     <input className="equal" type="button" value="=" onClick={Calculator} onChange={event => setOperator(event.target.value)}></input>
                 </form>
-
                 <form>
                     <input className="result" disabled defaultValue={result} readOnly/>
                 </form>
